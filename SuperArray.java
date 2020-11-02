631f3bc13860725d6a916045bc8c341691b6dd63
rifath33 <hrifath20@stuy.edu>
2020-11-02 05:21:55 -0500

    Hey I got size() and add() working

--- SuperArray.java
+++ SuperArray.java
@@ -1,3 +1,22 @@
 public class SuperArray{
+  private String[] data;               // this is a field
+  private int size; //The current size  // this is a field
+
+  public SuperArray(){
+    data = new String[10];
+  }
+
+  public int size(){
+    int counter = 0;
+    for (int i = 0; i < data.length; i ++)
+        if (data[i] != null)
+        counter ++;
+    return counter;
+  }
+
+  public boolean add(String element){
+    data[data.length-1] = element;
+    return(data[data.length-1] == element);
+  }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
 public class Tester{
   public static void main(String[] args){
 
-    
+    SuperArray Rifath = new SuperArray();
+    System.out.println(Rifath.size());
+    System.out.println(Rifath.add("kutha"));
+    System.out.println(Rifath.size());
 
   }
 }
