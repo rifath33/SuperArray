aa83ea6d51b748772264d039d9822cca57abc2fc
rifath33 <hrifath20@stuy.edu>
2020-11-12 17:58:49 -0500

    fin

--- Demo.java
+++ Demo.java
@@ -18,28 +18,72 @@ public class Demo{
       if(clone[i].equals("duplicate"))
       s.remove(i);
     }
+  }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    removeDuplicates(a);
+    removeDuplicates(b);
+
+    String[] aClone = a.toArray();
+    String[] bClone = b.toArray();
+    String[] smaller;
+    String[] other;
+    SuperArray smallerSuper;
+    SuperArray otherSuper;
+
+    int termsInCommon = 0;
+
+    int workingLength;
+
+    if(aClone.length < bClone.length)
+    {smaller = aClone;
+      smallerSuper = a;
+      other=bClone;
+      otherSuper = b;
+    }
+    else if ( bClone.length<aClone.length )
+    {smaller = bClone;
+      smallerSuper = b;
+      other=aClone;
+    otherSuper = a;}
+    else     {smaller = bClone;
+          smallerSuper = b;
+          other=aClone;
+        otherSuper = a;}
+
+    for(int i = 0; i<smaller.length; i++){
+      for(int f = 0; f<other.length; f++){
+        if(smaller[i].equals(other[f]))
+        termsInCommon++;
+      }
+    }
+
+    SuperArray result = new SuperArray(termsInCommon);
+    for(int i = 0; i<smaller.length; i++){
+        if(smallerSuper.contains(otherSuper.get(i)))
+        result.add(otherSuper.get(i));
+    }
+
+    return result;
 
   }
 
-  // public static SuperArray findOverlap(SuperArray a, SuperArray b){
-  //
-  // }
-  //
-  // public int lastIndexOf(String s){
-  //   for(int i = s.data)
-  // }
-
-
   public static void main(String[]args){
-    SuperArray words = new SuperArray();
-    //grouped to save vertical space
-    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
-    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
-    words.add("una");    words.add("ebi");     words.add("toro");
 
-    System.out.println(words);
-    removeDuplicates(words);
-    System.out.println(words);
+    SuperArray words = new SuperArray();
+//grouped to save vertical space
+words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+words.add("una");    words.add("ebi");     words.add("toro");
+
+SuperArray kutha = new SuperArray();
+//grouped to save vertical space
+kutha.add("kani");   kutha.add("uni");     kutha.add("ebi");     kutha.add("una");
+kutha.add("una");    kutha.add("ebi");     kutha.add("kani");    kutha.add("una");
+kutha.add("una");    kutha.add("ebi");     kutha.add("toro");
+
+    System.out.println(findOverlap(words,kutha));
+
   }
 
 }
