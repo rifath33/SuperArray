8c34a7759a4b01260a2f870efd0bd819c1e515b7
Rifath <hrifath20@stuy.edu>
2020-12-11 18:55:32 -0500

    konstantinovich test case

--- Demo.java
+++ Demo.java
@@ -8,6 +8,20 @@ public class Demo{
 				i--;
 			}
 
+      }
     }
+
+    public static void main(String[]args){
+    SuperArray words = new SuperArray();
+    //grouped to save vertical space
+    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("una");    words.add("ebi");     words.add("toro");
+
+    System.out.println(words);
+    removeDuplicates(words);
+    System.out.println(words);
     
+    }
+
 }
