e7d5553befce37dcb1504f0667f8c207a7edafc7
Rifath <hrifath20@stuy.edu>
2020-12-11 18:54:02 -0500

    made removeDuplicates

--- Demo.java
+++ Demo.java
@@ -1,3 +1,13 @@
 public class Demo{
-  
+    public static void removeDuplicates(SuperArray s){
+
+      for (int i = 0; i < s.size(); i++) {
+
+			if (i != s.indexOf(s.get(i))) {
+				s.remove(i);
+				i--;
+			}
+
+    }
+    
 }
