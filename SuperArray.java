15b2ada23b867c260e018c04d8057bbef3ceb0ef
Rifath <hrifath20@stuy.edu>
2020-12-10 21:00:22 -0500

    Starting off from working hw14 code

---
+++ SuperArray.java
@@ -0,0 +1,49 @@
+public class SuperArray{
+  private String[] data;
+  private int size; // the current size
+
+  public SuperArray(){
+    data = new String[10];
+    size = 0;
+  }
+
+  //depends on workign size
+  public int size(){
+    int counter = 0;
+    for(int i = 0; i < data.length; i++){
+      if(data[i]!=null)
+      counter++;
+    }
+    size = counter;
+    return size;
+  }
+
+  //depends on working size
+  public boolean add(String element){
+    if(size() == data.length)
+    resize();
+
+    data[size()] = element;
+    return(data[size()-1]==element);
+
+  }
+
+  public String get(int index){
+    return data[index];
+  }
+
+  public String set(int index, String element){
+    String memory = data[index];
+    data[index] = element;
+    return memory;
+  }
+
+  private void resize(){
+    String[] newArray = new String[size()*size()];
+    for(int i = 0; i < data.length; i++){
+      newArray[i] = data[i];
+    }
+    data = newArray;
+  }
+
+}
