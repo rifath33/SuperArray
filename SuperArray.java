f2b276a6dafda15009e15cd221310b8565784a8c
Rifath <hrifath20@stuy.edu>
2020-12-11 17:08:19 -0500

    i got indexOf to workgit add SuperArray.java

--- SuperArray.java
+++ SuperArray.java
@@ -113,4 +113,14 @@ public class SuperArray{
     return(counter>0);
   }
 
+  public int indexOf(String s){
+    for (int i = 0; i < size(); i ++) {
+      if (data[i].equals(s)){
+        return i;
+      }
+    }
+    return -1;
+
+  }
+
 }
