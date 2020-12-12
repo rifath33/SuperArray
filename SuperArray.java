c66ff792c6d4c02810166473b2d42756e8d309d0
Rifath <hrifath20@stuy.edu>
2020-12-11 19:58:21 -0500

    fixed HUGE issue withsize!

--- SuperArray.java
+++ SuperArray.java
@@ -17,44 +17,28 @@ public class SuperArray{
 
   //depends on workign size
   public int size(){
-    int counter = 0;
-    for(int i = 0; i < data.length; i++){
-      if(data[i]!=null)
-      counter++;
-    }
-    size = counter;
     return size;
   }
 
   //depends on working size
   public boolean add(String element){
-    if(size() == data.length)
-    resize();
-
-    data[size()] = element;
-    return(data[size()-1]==element);
+		if (data.length == size) resize();
+		data[size] = element;
+		size++;
+		return true;
   }
 
-  public boolean add(int index, String element){
+  public void add(int index, String element){
     if (index < 0 || index > size())
     throw new IndexOutOfBoundsException("Index " + index + " is out of range.");
 
-    if(index == size())
-    add(element);
-    else{
-      if(index < size() && size()>0){ // atleast 1 char in string
-        //use set method
-        add(get(size()-1)); //add last element again
-        for(int i = size()-1; i > index; i--){
-          set(i, get(i-1));
-        }
-        set(index, element);
-    }
-
-    }
-
-    return(data[index]==element);
-
+    if (data.length == size)
+    resize();
+		for (int i = size; i > index; i--) {
+			data[i] = data[i - 1];
+		}
+		data[index] = element;
+		size++;
   }
 
   public String remove(int index){
@@ -63,7 +47,7 @@ public class SuperArray{
     throw new IndexOutOfBoundsException("Index " + index + " is out of range.");
     String memory = get(index);
 		for (int i = index; i < size - 1; i++) {
-			data[i] = data[i+1];
+			set(i, get(i+1));
 		}
 		size--;
 		return memory;
