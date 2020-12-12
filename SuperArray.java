1bf02f363e20f4a70a8eae7fc03e79876b00a483
Rifath <hrifath20@stuy.edu>
2020-12-11 19:28:46 -0500

    sending just in case

--- SuperArray.java
+++ SuperArray.java
@@ -85,9 +85,8 @@ public class SuperArray{
   }
 
   public void clear(){
-    for(int i = 0; i < size(); i++){
-      data[i]=null;
-    }
+    String[] newData = new String[size()];
+    data = newData;
   }
 
   public boolean isEmpty(){
@@ -123,7 +122,10 @@ public class SuperArray{
   }
 
   public String[] toArray(){
-    String[] result = data;
+    String[] result = new String[size()];
+    for(int i = 0; i < size(); i++){
+      result[i] = get(i);
+    }
     return result;
   }
 
--- Tester.java
+++ Tester.java
@@ -1,12 +1,14 @@
-
-
 public class Tester{
   public static void main(String[] args){
 
     SuperArray Rifath = new SuperArray();
 
     Rifath.add("hello");
-    Rifath.add("hello");
+    Rifath.add("there");
+    Rifath.add("my");
+    Rifath.add("friend");
+    Rifath.clear();
+
     System.out.println(Rifath.toString());
   }
 }
