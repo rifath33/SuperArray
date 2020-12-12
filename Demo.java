62df584f54b14fc97301a7bf2bb6ed571b1897a1
Rifath <hrifath20@stuy.edu>
2020-12-11 19:12:03 -0500

    reusing findOverlap

--- Demo.java
+++ Demo.java
@@ -1,4 +1,5 @@
 public class Demo{
+
     public static void removeDuplicates(SuperArray s){
 
       for (int i = 0; i < s.size(); i++) {
@@ -11,6 +12,54 @@ public class Demo{
       }
     }
 
+    public static SuperArray findOverlap(SuperArray a, SuperArray b){
+      removeDuplicates(a);
+      removeDuplicates(b);
+
+      String[] aClone = a.toArray();
+      String[] bClone = b.toArray();
+      String[] smaller;
+      String[] other;
+      SuperArray smallerSuper;
+      SuperArray otherSuper;
+
+      int termsInCommon = 0;
+
+      int workingLength;
+
+      if(aClone.length < bClone.length)
+      {smaller = aClone;
+        smallerSuper = a;
+        other=bClone;
+        otherSuper = b;
+      }
+      else if ( bClone.length<aClone.length )
+      {smaller = bClone;
+        smallerSuper = b;
+        other=aClone;
+      otherSuper = a;}
+      else     {smaller = bClone;
+            smallerSuper = b;
+            other=aClone;
+          otherSuper = a;}
+
+      for(int i = 0; i<smaller.length; i++){
+        for(int f = 0; f<other.length; f++){
+          if(smaller[i].equals(other[f]))
+          termsInCommon++;
+        }
+      }
+
+      SuperArray result = new SuperArray(termsInCommon);
+      for(int i = 0; i<smaller.length; i++){
+          if(smallerSuper.contains(otherSuper.get(i)))
+          result.add(otherSuper.get(i));
+      }
+
+      return result;
+
+    }
+
     public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -21,7 +70,7 @@ public class Demo{
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
-    
+
     }
 
 }
