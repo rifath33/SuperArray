b137d3755f95ef26dc79dd6ed354f10f284978d2
Rifath <hrifath20@stuy.edu>
2020-12-11 19:29:24 -0500

    made zip

--- Demo.java
+++ Demo.java
@@ -23,6 +23,18 @@ public class Demo{
   		return result;
   	}
 
+    public static SuperArray zip(SuperArray a, SuperArray b){
+      SuperArray result = new SuperArray(a.size() + b.size());
+
+  		for (int i = 0; i < Math.max(a.size(), b.size()); i++) {
+  			if (i < a.size())
+        result.add(a.get(i));
+  			if (i < b.size())
+        result.add(b.get(i));
+  		}
+  		return result;
+    }
+
     public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
